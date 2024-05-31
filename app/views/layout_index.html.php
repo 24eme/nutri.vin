1205367390d4b87693855a5b367e87775c83b906
Tangui Morlier <tangui@tangui.eu.org>
2024-05-31 17:10:03 +0200

    Plateformes

--- app/views/layout_index.html.php
+++ app/views/layout_index.html.php
@@ -30,7 +30,7 @@
                     <li><a href="#" class="nav-link px-2">Les avantages</a></li>
                     <li><a href="#" class="nav-link px-2">Les fonctionnalités</a></li>
                     <li><a href="#" class="nav-link px-2">Conformité UE</a></li>
-                    <li><a href="#" class="nav-link px-2">Les instances</a></li>
+                    <li><a href="#" class="nav-link px-2">Les plateforme</a></li>
                     <li><a href="#" class="nav-link px-2">À propos</a></li>
                 </ul>
 
@@ -97,7 +97,7 @@
                             </div>
                             <div class="card-body">
                                 <h5 class="card-title">L'open source au service de la durabilité</h5>
-                                <p class="card-text">Le projet peut être utilisé sur nutri.vin directement, sur une instance mis à disposition par une interprofession ou une ODG, ou librement installé sur son propre serveur et nom de domaine, pour avoir la maitrîse du QR Code dans le temps.</p>
+                                <p class="card-text">Le projet peut être utilisé sur nutri.vin directement, sur une plateforme mis à disposition par une interprofession ou une ODG, ou librement installé sur son propre serveur et nom de domaine, pour avoir la maitrîse du QR Code dans le temps.</p>
                             </div>
                         </div>
                     </div>
@@ -108,7 +108,7 @@
                             </div>
                             <div class="card-body">
                                 <h5 class="card-title">Pérénité</h5>
-                                <p class="card-text">Le projet ne dépend pas d'une seule entreprise privée et de sa viabilité économique. Il est financé par une interprofession et une entreprise coopérative. Les QRCodes déjà créés peuvent être facilement migré sur une autre instance grace à la fédération.</p>
+                                <p class="card-text">Le projet ne dépend pas d'une seule entreprise privée et de sa viabilité économique. Il est financé par une interprofession et une entreprise coopérative. Les QRCodes déjà créés peuvent être facilement migré sur une autre plateforme grace à la fédération.</p>
                             </div>
                         </div>
                     </div>
@@ -233,14 +233,14 @@
                     <li class="list-group-item"><i class="bi bi-patch-check text-success"></i> Aucune information à caractère personnel n'est collectée</li>
                     <li class="list-group-item"><i class="bi bi-patch-check text-success"></i> Les utilisateurs ne sont pas tracés</li>
                     <li class="list-group-item"><i class="bi bi-patch-check text-success"></i> Aucun cookie créé lors de la consultation d'une fiche</li>
-                    <li class="list-group-item"><i class="bi bi-patch-check text-success"></i> Consultation sans recours à un services tiers</li>
+                    <li class="list-group-item"><i class="bi bi-patch-check text-success"></i> Consultation sans recours à un service tiers</li>
                     <li class="list-group-item"><i class="bi bi-patch-check text-success"></i> Pas d'informations commerciales</li>
                 </div>
             </div>
         </div>
         <div class="bg-light" style="border-bottom: 1px solid #ddd">
             <div class="container pt-4 pb-5" style="max-width: 1050px;">
-                <h2 class="mt-4">Les instances existantes</h2>
+                <h2 class="mt-4">Les plateformes existantes</h2>
 
                 <p>Le QRCode généré va utiliser un nom de domaine, ce choix de nom de domaine est un choix important, pour que le QR Code dure dans le temps.<br /><br />
 
@@ -251,7 +251,7 @@
                 <table class="table table-striped table-bordered mt-4">
                     <thead>
                         <tr>
-                            <th>Instance</th>
+                            <th>Plateforme</th>
                             <th>Gestionnaire</th>
                             <th>Description</th>
                             <th>Coût</th>
