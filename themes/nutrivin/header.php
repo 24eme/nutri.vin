50219cee8269baddffda52aef328d9029786ffa4
Tangui Morlier <tangui@tangui.eu.org>
2024-06-03 19:31:35 +0200

    Wrong link

--- themes/nutrivin/header.php
+++ themes/nutrivin/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container">
-  <a class="navbar-brand" href="/qrcode/userid/list"><img src="/images/logo.svg" class="mx-auto d-block" alt="Logo nutrivin" width="42" height="42"></a>
+  <a class="navbar-brand" href="/qrcode"><img src="/images/logo.svg" class="mx-auto d-block" alt="Logo nutrivin" width="42" height="42"></a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
