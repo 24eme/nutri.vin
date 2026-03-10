ac054a13e1b0b3730e9c1be80028f5a3cc116f1a
Gabriel Poma <gpoma@24eme.fr>
2026-03-10 17:27:47 +0100

    tableau pour listing utilisateur

--- app/views/admin_users.html.php
+++ app/views/admin_users.html.php
@@ -1,9 +1,17 @@
 <h1>Les utilisateurs ayant créés des QRCodes</h1>
 
-<ul>
-<?php foreach($users as $u => $n) {
-    echo "<p><a href='$urlbase/qrcode/$u/list'>$u - $n</a></p>";
-} ?>
-</ul>
+<table class="table table-striped table-bordered">
+  <thead>
+    <th>Domaine</th>
+  </thead>
+  <tbody>
+    <?php foreach($users as $u => $n) : ?>
+      <tr>
+        <td>
+          <a href='<?php echo "$urlbase/qrcode/$u/list" ?>'><?php echo "$n ($u)" ?></a>
+        </td>
+      </tr>
+    <?php endforeach ?>
+</table>
 
 <p class="text-end"><a href="<?php echo $urlbase; ?>/qrcode" class="btn btn-success">Accéder à mon espace</a></p>
