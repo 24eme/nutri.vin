647c1c11a0c5cbcf42d70e42e5bc13633e998d45
Gabriel Poma <gpoma@24eme.fr>
2026-03-12 12:55:20 +0100

    update vue des stats pour couchdb

--- docs/couchdb/stats.visites.view.js
+++ docs/couchdb/stats.visites.view.js
@@ -1,4 +1,13 @@
 function (doc) {
-  if (!doc.visites_nb) { return }
-  emit([doc._id.substring(0, 1 + parseInt(doc._id.substring(0, 1))), doc.user_id, doc.domaine_nom, doc._id], [1, doc.visites_nb]);
+  if (! doc.hasOwnProperty('visites')) { return }
+
+  var visites = 0;
+
+  if (doc.hasOwnProperty('visites_nb')) {
+    visites = doc.visites_nb;
+  } else {
+    visites = doc.visites ? JSON.parse(doc.visites).length : 0;
+  }
+
+  emit([doc._id.substring(0, 1 + parseInt(doc._id.substring(0, 1))), doc.user_id, doc.responsable_nom, doc._id], [1, visites]);
 }
