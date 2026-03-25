af821858c9abc8f100b9c3f49ab770f26d9c64cf
Gabriel Poma <gpoma@24eme.fr>
2026-03-25 16:46:53 +0100

    on déplace dans le modèle l'export

--- app/models/QRCode.class.php
+++ app/models/QRCode.class.php
@@ -833,4 +833,77 @@ class QRCode extends Mapper
         return $images;
     }
 
+    public static function exportToCsv()
+    {
+        $URLBASE = Config::getInstance()->getUrlbase().'/';
+
+        $csv_unsorted = new \SplFileObject('php://temp', 'rw');
+        $csv_unsorted->setCsvControl(';');
+
+        foreach (QRCode::findAll(false) as $qrcode) {
+            $csv_unsorted->fputcsv([
+                $qrcode->getId(),
+                $qrcode->user_id,
+                $qrcode->responsable_nom,
+                $qrcode->responsable_siret,
+                $qrcode->denomination,
+                $qrcode->couleur,
+                $qrcode->domaine_nom,
+                $qrcode->cuvee_nom,
+                $qrcode->alcool_degre,
+                $qrcode->centilisation,
+                $qrcode->millesime,
+                $qrcode->lot,
+                implode(', ', json_decode($qrcode->labels)),
+                $qrcode->denomination_instance ? 'NON' : 'OUI', // facturable ?
+                $URLBASE.$qrcode->getId(),
+            ]);
+        }
+
+        $users_ids = [];
+        $csv_unsorted->rewind();
+        while (! $csv_unsorted->eof()) {
+            $users_ids[] = $csv_unsorted->fgetcsv()[1];
+        }
+        $csv_unsorted->rewind();
+
+        $users_ids = array_unique($users_ids);
+        sort($users_ids);
+
+        ob_flush();
+
+        $csv = new \SplFileObject('php://output', 'w');
+        $csv->setCsvControl(';');
+
+        $csv->fputcsv([
+            'QRCode',
+            'ID Utilisateur',
+            'Nom responsable',
+            'Siret responsable',
+            'Dénomination',
+            'Couleur',
+            'Domaine',
+            'Cuvée',
+            'Volume d\'alcool',
+            'Centilisation',
+            'Millésime',
+            'Lot',
+            'Labels',
+            'Facturable',
+            'Lien',
+        ]);
+
+        foreach ($users_ids as $user_id) {
+            while (! $csv_unsorted->eof()) {
+                $row = $csv_unsorted->fgetcsv();
+                if ($row[1] === $user_id) {
+                    $csv->fputcsv($row);
+                }
+
+                $csv->fflush();
+            }
+
+            $csv_unsorted->rewind();
+        }
+    }
 }
